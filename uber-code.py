abc0be02981b12ec5cbc5d892b187f33e10f5963
Dustin Parkinson <dustin@streamlit.com>
2021-05-26 10:49:08 -0600

    added map

--- uber-code.py
+++ uber-code.py
@@ -4,7 +4,7 @@ import pandas as pd
 import numpy as np
 
 st.title('Uber pickups in NYC')
-st.subheader('Number of pickups by hour')
+
 
 DATE_COLUMN = 'date/time'
 DATA_URL = ('https://s3-us-west-2.amazonaws.com/'
@@ -31,4 +31,8 @@ st.write(data)
 
 hist_values = np.histogram(
     data[DATE_COLUMN].dt.hour, bins=24, range=(0,24))[0]
+st.subheader('Number of pickups by hour')   
 st.bar_chart(hist_values)
+
+st.subheader('Map of all pickups')
+st.map(data)
